7a9ef8b59821e83166b23420f548e5151da965da
Anil <anil6996f@gmail.com>
2024-12-31 22:04:15 +0530

    added registration for user along with account activation via code in mail

--- src/main/java/com/anil/BookApp/BookAppApplication.java
+++ src/main/java/com/anil/BookApp/BookAppApplication.java
@@ -1,15 +1,29 @@
 package com.anil.BookApp;
 
+import com.anil.BookApp.role.Role;
+import com.anil.BookApp.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
---
+++ src/main/java/com/anil/BookApp/auth/AuthenticationService.java
@@ -0,0 +1,76 @@
+package com.anil.BookApp.auth;
+
+import com.anil.BookApp.email.EmailService;
+import com.anil.BookApp.email.EmailTemplateName;
+import com.anil.BookApp.role.RoleRepository;
+import com.anil.BookApp.user.Token;
+import com.anil.BookApp.user.TokenRepository;
+import com.anil.BookApp.user.User;
+import com.anil.BookApp.user.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest request) throws MessagingException {
+        var userRole = roleRepository.findByName("USER").orElseThrow(()->new IllegalStateException("Role user was not initialized"));
+        var user = User.builder().
+                firstName(request.getFirstname()).
+                lastName(request.getLastname()).
+                email(request.getEmail()).
+                password(passwordEncoder.encode(request.getPassword())).
+                accountLocked(false).
+                enabled(false).
+                roles(List.of(userRole)).
+                build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(user.getEmail(), user.fullName(), EmailTemplateName.ACTIVATE_ACCOUNT,activationUrl,newToken,"Account activation");
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder().
+                token(generatedToken).
+                createdAt(LocalDateTime.now()).
+                expiresAt(LocalDateTime.now().plusMinutes(15)).
+                user(user).
+                build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+}
