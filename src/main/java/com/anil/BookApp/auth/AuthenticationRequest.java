60058ca300da126102fc49150ee2dcfcc92f7679
Anil <anil6996f@gmail.com>
2025-01-01 11:31:41 +0530

    added login and account activation functionality

---
+++ src/main/java/com/anil/BookApp/auth/AuthenticationRequest.java
@@ -0,0 +1,25 @@
+package com.anil.BookApp.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationRequest {
+
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8,message = "Password must be greater than 8 characters")
+    private String password;
+}
