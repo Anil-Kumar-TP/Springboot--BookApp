cdb254b43c97a7af3a93242af207e1580aae0349
Anil <anil6996f@gmail.com>
2025-01-02 17:16:13 +0530

    implemented functionality to enable users to borrow books

--- src/main/java/com/anil/BookApp/book/BookController.java
+++ src/main/java/com/anil/BookApp/book/BookController.java
@@ -56,5 +56,8 @@ public class BookController {
         return ResponseEntity.ok(service.updateArchivedStatus(bookId,connectedUser));
     }
 
-
+    @PostMapping("/borrow/{book-id}")
+    public ResponseEntity<Integer> borrowBook(@PathVariable("book-id") Integer bookId,Authentication connectedUser){
+        return ResponseEntity.ok(service.borrowBook(bookId,connectedUser));
+    }
 }
--- src/main/java/com/anil/BookApp/book/BookService.java
+++ src/main/java/com/anil/BookApp/book/BookService.java
@@ -89,4 +89,26 @@ public class BookService {
         bookRepository.save(book);
         return bookId;
     }
+
+    public Integer borrowBook(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId).orElseThrow(()->new EntityNotFoundException("Mo book found with Id :" + bookId));
+        if (book.isArchived() || !book.isShareable()){
+            throw new OperationNotPermittedException("You cannot borrow archived / non-shareable books");
+        }
+        User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getOwner().getId(),user.getId())){
+            throw new OperationNotPermittedException("You cannot borrow your own book");
+        }
+        final boolean isAlreadyBorrowed = bookTransactionHistoryRepository.isAlreadyBorrowedByUser(bookId,user.getId());
+        if (isAlreadyBorrowed){
+            throw new OperationNotPermittedException("Book was already borrowed");
+        }
+        BookTransactionHistory bookTransactionHistory = BookTransactionHistory.builder().
+                                                        user(user).
+                                                        book(book).
+                                                        returned(false).
+                                                        returnApproved(false).
+                                                        build();
+        return bookTransactionHistoryRepository.save(bookTransactionHistory).getId();
+    }
 }
